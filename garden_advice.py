554763f1186605c10e5117a4151a656692418227
Will Tuckwell <willtuckwell.1@gmail.com>
2026-09-17 11:27:10 +0100

    Add files via upload

---
+++ garden_advice.py
@@ -0,0 +1,10 @@
+def garden_advice():
+    print("Water plants regularly.")
+    print("Use compost to improve soil quality.")
+
+    # TODO: Add advice based on the season.
+    # TODO: Add support for different plant types.
+
+
+if __name__ == "__main__":
+    garden_advice()
